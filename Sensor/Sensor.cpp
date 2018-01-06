09201378565279a9ef45fc0acf08d55143588d1e
Jiahao Li <reg@ljh.me>
2018-01-06 16:06:33 +0800

    Blink on steroid test

--- Sensor/Sensor.cpp
+++ Sensor/Sensor.cpp
@@ -1,9 +1,14 @@
 #include <Arduino.h>
 
-void setup() {
+const int LED_PIN = 13;
 
+void setup() {
+    pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
-    
+    digitalWrite(LED_PIN, HIGH);
+    delay(100);
+    digitalWrite(LED_PIN, LOW);
+    delay(100);
 }
