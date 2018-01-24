33a772ad334ab1708ce918535edddba39953af96
Jiahao Li <reg@ljh.me>
2018-01-25 01:37:40 +0800

    Power optimizations

--- Sensor/Sensor.cpp
+++ Sensor/Sensor.cpp
@@ -9,52 +9,71 @@
 #include "ZClient.h"
 #include "Zigbee.h"
 
+const int PIN_NETWORK_LED = 0;
+const int PIN_INPUT = 1;
+
 const int PIN_ZIGBEE_TX = 3;
 const int PIN_ZIGBEE_RX = 4;
 
-const int PIN_INPUT = 1;
-const int PIN_LED = 0;
+const uint8_t WDT_SLEEP_FLAG = _BV(WDP3) | _BV(WDP0);
+const size_t WDT_SLEEP_MULTIPLIER = 2;
 
 ZClient client(PIN_ZIGBEE_TX, PIN_ZIGBEE_RX);
 
-ISR(WDT_vect) {}
+void deepSleep(uint8_t timeFlag) {
+  // Disables pin change interrupt  and pin output during deep sleep
+  GIMSK &= ~_BV(PCIE);
+
+  wdt_reset();
+  MCUSR = 0;
+  WDTCR |= (_BV(WDCE) | _BV(WDE));
+  WDTCR = _BV(WDIE) | timeFlag;
+
+  sei();
+  sleep_enable();
+  sleep_cpu();
+  sleep_disable();
+  // sleep_mode();
+
+  GIMSK |= _BV(PCIE);
+}
+
+ISR(WDT_vect) {
+  static int counter = 0;
+
+  counter++;
+
+  if (counter == WDT_SLEEP_MULTIPLIER) {
+    counter = 0;
+  } else {
+    deepSleep(WDT_SLEEP_FLAG);
+  }
+}
 
 void setup() {
-  DDRB |= 4;
+  DDRB |= _BV(PIN_NETWORK_LED);
 
   Tick::begin();
   client.begin();
 
   ADCSRA &= ~_BV(ADEN);
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
 }
 
 void loop() {
   client.bee.wakeUp();
 
   if (client.checkReadyStatus()) {
-    PORTB |= 4;
+    PORTB |= _BV(PIN_NETWORK_LED);
   } else {
-    PORTB &= ~4;
+    PORTB &= ~_BV(PIN_NETWORK_LED);
   }
 
   if (client.ready) {
     client.broadcast("ping", "type", "fake_light_bulb");
   }
 
-  // Watchdog sleep section
-
-  GIMSK &= ~_BV(PCIE);
-
-  wdt_reset();
-  MCUSR = 0;
-  WDTCR |= (_BV(WDCE) | _BV(WDE));
-  WDTCR = (_BV(WDIE) | 0b000110);
-
-  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-  sei();
-  sleep_mode();
-
-  GIMSK |= _BV(PCIE);
+  deepSleep(WDT_SLEEP_FLAG);
 }
 
 int main() {
--- Sensor/Zigbee.cpp
+++ Sensor/Zigbee.cpp
@@ -7,9 +7,9 @@
 #include <string.h>
 
 const int Zigbee::WAKEUP_MESSAGE_LENGTH = 10;
-const int Zigbee::WAKEUP_WAIT_TIME_INITIAL = 40;
+const int Zigbee::WAKEUP_WAIT_TIME_INITIAL = 100;
 const int Zigbee::WAKEKE_WAIT_TIME_BACKOFF_FACTOR = 2;
-const int Zigbee::WAKEUP_WAIT_TIME_MAX = 80;
+const int Zigbee::WAKEUP_WAIT_TIME_MAX = 200;
 const int Zigbee::WAKEUP_MAX_TRIES = 7;
 const int Zigbee::QUERY_WAIT_TIME = 500;
 const int Zigbee::MESSAGE_GAP_DELAY = 20;
