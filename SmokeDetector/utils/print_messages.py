81ba8727001278a942fed1c7f19c5c506b322532
Jiahao Li <reg@ljh.me>
2018-02-19 22:43:15 -0500

    Adds startup sequences to print_messages.py

--- SmokeDetector/utils/print_messages.py
+++ SmokeDetector/utils/print_messages.py
@@ -60,11 +60,23 @@ class ListeningThread(threading.Thread):
                 (cmd_type, bytes_to_string(cmd), bytes_to_string(data)))
 
 if sys.argv[1] == 'master':
-    DEV = '/dev/cu.wchusbserial401220'
+    DEV = '/dev/cu.wchusbserial401120'
 elif sys.argv[1] == 'slave':
     DEV = '/dev/cu.usbserial-A9M9DV3R'
 else:
     raise RuntimeError("Invalid device given.")
+
+def send_command(s, cmd):
+    log('Sending %s' % bytes_to_string(cmd))
+
+    length = len(cmd) - 2
+
+    s.write(bytes([0xFE]))
+    s.write(bytes([length]))
+    s.write(cmd)
+    s.write(bytes([calculate_checksum(bytes([length]) + cmd)]))
+
+    time.sleep(0.1)
 
 with serial.Serial(DEV, 115200) as s:
     time.sleep(1)
@@ -72,15 +84,50 @@ with serial.Serial(DEV, 115200) as s:
     listener = ListeningThread(s)
     listener.start()
 
+    if len(sys.argv) > 2:
+        if sys.argv[2] == 'coord':
+            send_command(s, b'\x21\x09\x03\x00\x00\x01\x03')
+            send_command(s, b'\x41\x00\x00')
+
+            time.sleep(3)
+
+            send_command(s, b'\x21\x09\x87\x00\x00\x01\x00')
+            send_command(s, b'\x2F\x08\x01\x00\x20\x00\x00')
+            send_command(s, b'\x2F\x08\x00\x00\x00\x00\x00')
+            send_command(s, b'\x2F\x05\x04')
+
+            time.sleep(3)
+
+            send_command(s, b'\x21\x09\x8F\x00\x00\x01\x01')
+            send_command(s, b'\x2F\x05\x02')
+
+            time.sleep(1)
+
+            send_command(s, b'\x24\x00\x01\x04\x01\x00\x01\x00\x00\x02\x00\x00\x06\x00\x00\x00\x06\x00')
+        elif sys.argv[2] == 'router':
+            send_command(s, b'\x21\x09\x03\x00\x00\x01\x03')
+            send_command(s, b'\x41\x00\x00')
+
+            time.sleep(3)
+
+            send_command(s, b'\x21\x09\x87\x00\x00\x01\x01')
+            send_command(s, b'\x2F\x08\x01\x00\x20\x00\x00')
+            send_command(s, b'\x2F\x08\x00\x00\x00\x00\x00')
+            send_command(s, b'\x2F\x05\x02')
+
+            time.sleep(5)
+
+            send_command(s, b'\x24\x00\x01\x04\x01\x00\x01\x00\x00\x02\x00\x00\x06\x00\x00\x00\x06\x00')
+        else:
+            raise RuntimeError("Invalid startup sequence.")
+
+    log('Accepting manual commands')
+
     for line in sys.stdin:
         tokens = line.split()
         tokens = filter(lambda x: len(x) == 2, tokens)
         bs = bytes(map(lambda x: int(x, 16), tokens))
-        length = len(bs) - 2
 
-        s.write(bytes([0xFE]))
-        s.write(bytes([length]))
-        s.write(bs)
-        s.write(bytes([calculate_checksum(bytes([length]) + bs)]))
+        send_command(s, bs)
 
     listener.join()
