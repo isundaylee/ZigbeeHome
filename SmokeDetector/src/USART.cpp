d175cc94e3c96d05f63a95b1c9d38764e76b5025
Jiahao Li <reg@ljh.me>
2018-03-23 18:20:26 -0400

    Fixes USART overrun hang

--- SmokeDetector/src/USART.cpp
+++ SmokeDetector/src/USART.cpp
@@ -4,4 +4,9 @@ extern "C" void vector_usart2() {
   while (USART_2::usart()->ISR & USART_ISR_RXNE) {
     USART_2::rxBuffer_.push(static_cast<uint8_t>(USART_2::usart()->RDR));
   }
+
+  if (USART_2::usart()->ISR & USART_ISR_ORE) {
+    USART_2::flush();
+    USART_2::usart()->ICR |= USART_ICR_ORECF;
+  }
 }
